1971e70c6f318761e18e0735a47d6b7b03bbf97d
Hardikk Kamboj <64458111+hady68@users.noreply.github.com>
2022-11-23 17:45:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+let homeScoreb1 = document.getElementById("home1")
+let homeStoreEl =document.getElementById("home-score")
+let guestStoreEl =document.getElementById("guest-score")
+let homeScore = 0
+let guestScore =0
+
+function updateHomescore(){
+     homeStoreEl.textContent = homeScore
+}
+
+function addhome1(){
+    homeScore += 1
+   updateHomescore();
+}
+
+function addhome2(){
+    homeScore += 2
+   updateHomescore();
+}
+
+function addhome3(){
+    homeScore += 3
+   updateHomescore();
+}
+
+function updateGuestscore(){
+     guestStoreEl.textContent = guestScore
+}
+
+function addguest1(){
+    guestScore += 1
+   updateGuestscore();
+}
+
+function addguest2(){
+    guestScore += 2
+   updateGuestscore();
+}
+
+function addguest3(){
+    guestScore += 3
+   updateGuestscore();
+}
